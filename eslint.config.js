e3ad5ce90fb14786ab7e1536bd71f504cef10a4a
butuzz74@gmail.com <butuzz74@gmail.com>
2025-03-05 22:11:54 +0300

    reinstall

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
-import js from "@eslint/js";
 import next from "eslint-plugin-next";
 
 export default [
-  js.configs.recommended,
-  next.configs.recommended,
+  {
+    ignores: ["node_modules", "dist"],
+  },
+  next(),
 ];
